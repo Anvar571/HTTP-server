cf83a674511492560ecb4a605097053b9872cb02
anvar571 <abdurashidovanvar52@gmail.com>
2023-11-06 16:23:42 +0500

    not found page and docs

--- js/main.js
+++ js/main.js
@@ -33,60 +33,26 @@ const helloHtmlFunction = () => {
   )
 }
 
-const fileResponse = (bytesRead, buffer) => {
-  return (
-    'HTTP/1.1 200 OK\r\n' +
-    'Content-Type: application/octet-stream\r\n' +
-    `Content-Length: ${bytesRead}\r\n\r\n${buffer}`
-  )
-}
-
 const sendJsonData = (body) => {
   return (
     `HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: ${Buffer.byteLength(body)}\r\n\r\n${body}`
   )
 }
 
-const fileCreatedResponse = () => {
+const notFoundResponse = (body) => {
   return (
-    'HTTP/1.1 201 Created\r\n\r\n'
-  )
-}
+    `HTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\nContent-Length: ${Buffer.byteLength(body)}\r\n\r\n${body}`
 
-const notFoundResponse = () => {
-  return 'HTTP/1.1 404 Not Found\r\n\r\n'
+  )
 }
 
 const handlers = {
   '/echo': function () {
-    const [, text] = this.headers.path.match(/\/echo\/(.+)/)
-
+    const text = 'lorem ipsum';
     this.socket.write(textResponse(text), 'utf-8')
   },
   '/user-agent': function () {
     this.socket.write(textResponse(this.headers.ua), 'utf-8')
-  },
-  '/files': function () {
-    const [, filename] = this.headers.path.match(/\/files\/(.+)/)
-
-    const dirFlag = process.argv.indexOf('--directory') + 1
-    const directory = process.argv[dirFlag]
-
-    const filePath = path.join(directory, filename)
-
-    try {
-      if (this.headers.method === 'GET') {
-        const fd = fs.openSync(filePath, 'r')
-        const contents = fs.readFileSync(fd)
-
-        this.socket.write(fileResponse(contents.length, contents))
-      } else if (this.headers.method === 'POST') {
-        fs.writeFileSync(filePath, this.headers.data)
-        this.socket.write(fileCreatedResponse())
-      }
-    } catch (error) {
-      this.socket.write(notFoundResponse())
-    }
   },
   '/': function () {
     this.socket.write(firstFunction(), 'utf-8')
@@ -118,7 +84,12 @@ const server = net.createServer((socket) => {
     const handler = getHandler(headers.path, handlers)
 
     if (!handler) {
-      socket.write(notFoundResponse())
+      const body = {
+        status: 404,
+        message: 'Not found endpoint'
+      };
+      const jsonData = JSON.stringify(body);
+      socket.write(notFoundResponse(jsonData))
       socket.end();
 
       return;
